6588ef79d31e8c9e255d0db220ae60f542ae54b6
Krystof Dolejsi <KrystofDolejsi@gmail.com>
2021-09-25 12:12:59 +0200

    switched to using function over classes in react

--- music_controller/frontend/webpack.config.js
+++ music_controller/frontend/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
@@ -23,10 +24,10 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env": {
-        // This has effect on the react lib size
-        NODE_ENV: JSON.stringify("production"),
-      },
+        'process.env.NODE_ENV' : JSON.stringify('development')
+    }),
+    new webpack.ProvidePlugin({
+      "React": "react",
     }),
   ],
 };
--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("api.urls"))
+    path("api/", include("api.urls")),
+    path("", include("frontend.urls")),
 ]
